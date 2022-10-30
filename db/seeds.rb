36c089020458fc371c53392b52647621ea73cac0
王静怡 <carolinewang@ouseis-air.mynetworksettings.com>
2022-10-30 15:51:57 -0400

    added seeds.rb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+activities = [{:event_name => 'Halloween Night', :organizer => 'Caroline Wang', :max_size => 10, :current_size =>3, :category => 'Entertainment', :date => '31-Oct-2022'},
+{:event_name => 'Met Sunday', :organizer => 'Liting Huang', :max_size => 4, :current_size =>1, :category => 'Arts', :date => '30-Oct-2022'}
+]
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
-      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
-      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
-  	 ]
-
-movies.each do |movie|
-  Movie.create!(movie)
+activities.each do |activity|
+  Activity.create!(activity)
 end
