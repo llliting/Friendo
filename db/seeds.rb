9613016852d7736017821657c408c55ed8c154c1
王静怡 <carolinewang@ouseis-air.mynetworksettings.com>
2022-10-30 17:16:12 -0400

    html changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,6 @@ activities = [{:event_name => 'Halloween Night', :organizer => 'Caroline Wang', 
 {:event_name => 'Met Sunday', :organizer => 'Liting Huang', :max_size => 4, :current_size =>1, :category => 'Arts', :date => '30-Oct-2022'}
 ]
 
-<<<<<<< HEAD
-activities = [{:event_name => 'Halloween Night', :organizer => 'Caroline Wang', :max_size => 10, :current_size =>3, :category => 'Entertainment', :date => '31-Oct-2022'},
-{:event_name => 'Met Sunday', :organizer => 'Liting Huang', :max_size => 4, :current_size =>1, :category => 'Arts', :date => '30-Oct-2022'}
-]
-
-=======
->>>>>>> 36c089020458fc371c53392b52647621ea73cac0
 activities.each do |activity|
   Activity.create!(activity)
 end
