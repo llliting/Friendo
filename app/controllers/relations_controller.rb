8ec1f676c812101e75bf3a9ffa94b3a5cd16b272
llliting <lh3119@columbia.edu>
2022-11-13 01:38:42 -0500

    added capacity checking feature

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -2,12 +2,25 @@ class RelationsController < ApplicationController
     def create
         if ActivityUserRelation.where(user_id: params[:user_id], activity_id: params[:activity_id]).empty?
             ActivityUserRelation.create!(user_id: params[:user_id], activity_id: params[:activity_id])
+            @activity = Activity.find(params[:activity_id])
+            if @activity[:current_size] < @activity[:max_size]
+                @activity.increment! (:current_size)
+            else
+                flash[:notice] = "The activity has reached the maximum number of participants."
+            end
         end
         redirect_to activities_path
     end
 
     def destroy
         ActivityUserRelation.where(user_id: params[:user_id], activity_id: params[:activity_id]).destroy_all
+        @activity = Activity.find(params[:activity_id])
+        if @activity[:current_size] > 1
+            @activity.decrement! (:current_size)
+        else
+            ####close activity ??
+        end
+        ### DECREASE SIZE
         redirect_to activities_path
     end
 end
